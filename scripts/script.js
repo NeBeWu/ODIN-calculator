9d424e92c6a1136d7c8cd6846a60825de4c69147
nebewu <74262987+NeBeWu@users.noreply.github.com>
2021-10-08 16:23:03 -0300

    Implement operations logic
    Create add, subtract, multiply and divide functions, and a operate
    function.
    
    The add, subtract, multiply and divide functions are used to
    execute the four basic calculator operations. The operate function
    is responsible to change the string inputs to number and assign
    the right function to the chosen operator.

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+function operate(operator, number1, number2) {
+  number1 = Number(number1);
+  number2 = Number(number2);
+
+  switch (operator) {
+    case '+':
+      return add(number1, number2);
+    case '−':
+      return subtract(number1, number2);
+    case '×':
+      return multiply(number1, number2);
+    case '÷':
+      return divide(number1, number2);
+  }
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
