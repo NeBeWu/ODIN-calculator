1807679a9ff529814f29021b45e7469a84991af7
nebewu <74262987+NeBeWu@users.noreply.github.com>
2021-10-10 13:28:35 -0300

    Implement display and basic calculator functions
    Rename display classes and add an alignment class to the display
    elements, and add more specific button classes to the buttons
    elements. Change delete button color to yellow, operator buttons
    color to blue, add margin, alignment to display elements, increase
    font-size for current-display, add scroll overflow to past-display
    and refactor the css code. Create global variables for the first
    and second number of a operation and for the operator, create
    global constants to store all buttons and displays, implement
    functions to change the global variables, reset them and change
    the displays. Add event listeners for the numbers, operations and
    equals buttons and implement an approximation function.
    
    The addition of classes to the html elements help customize and
    add functionality to them. The customization of these elements
    improve the user interface. The addition of global variables and
    constants facilitates the calculator logic implementation and the
    update functions add responsiveness to the calculator logic. The
    event listeners enables a precise control of how button press
    interact with the logic and the approximation function restricts
    the computations to 10 decimal places.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,75 @@
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+const currentDisplay = document.querySelector('.current-display');
+const pastDisplay = document.querySelector('.past-display');
+const numberButton = document.querySelectorAll('.number-button');
+const operatorButton = document.querySelectorAll('.operator-button');
+const equalsButton = document.querySelector('.equals-button');
+const decimalButton = document.querySelector('.decimal-button');
+const clearButton = document.querySelector('.clear-button');
+const deleteButton = document.querySelector('.delete-button');
+
+function updateCurrentDisplay() {
+  currentDisplay.textContent = firstNumber + operator + secondNumber;
+}
+
+function updatePastDisplay(value) {
+  const content = document.createElement('p');
+
+  content.textContent = firstNumber + operator + secondNumber + `=${value}`;
+  content.style.marginRight = '5vw';
+
+  pastDisplay.prepend(content);
+}
+
+function updateFirstNumber(value) {
+  firstNumber += value;
+  updateCurrentDisplay();
+}
+
+function updateSecondNumber(value) {
+  secondNumber += value;
+  updateCurrentDisplay();
+}
+
+function updateOperator(value) {
+  operator = value;
+  updateCurrentDisplay();
+}
+
+function resetCurrentVariables() {
+  firstNumber = '';
+  secondNumber = '';
+  operator = '';
+}
+
+numberButton.forEach((currentValue) => {
+  currentValue.addEventListener('click', () => {
+    if (operator == '') {
+      updateFirstNumber(currentValue.textContent);
+    } else {
+      updateSecondNumber(currentValue.textContent);
+    }
+  });
+});
+
+operatorButton.forEach((currentValue) => {
+  currentValue.addEventListener('click', () => {
+    if (firstNumber != '' && secondNumber == '') {
+      updateOperator(currentValue.textContent);
+    }
+  });
+});
+
+equalsButton.addEventListener('click', () => {
+  const value = operate(operator, firstNumber, secondNumber);
+  updatePastDisplay(value);
+  resetCurrentVariables();
+  updateFirstNumber(`${value}`);
+});
+
 function operate(operator, number1, number2) {
   number1 = Number(number1);
   number2 = Number(number2);
@@ -11,21 +83,27 @@ function operate(operator, number1, number2) {
       return multiply(number1, number2);
     case '÷':
       return divide(number1, number2);
+    default:
+      return null;
   }
 }
 
 function add(number1, number2) {
-  return number1 + number2;
+  return approximate(number1 + number2);
 }
 
 function subtract(number1, number2) {
-  return number1 - number2;
+  return approximate(number1 - number2);
 }
 
 function multiply(number1, number2) {
-  return number1 * number2;
+  return approximate(number1 * number2);
 }
 
 function divide(number1, number2) {
-  return number1 / number2;
+  return approximate(number1 / number2);
+}
+
+function approximate(number) {
+  return Math.trunc(10000000000 * number) / 10000000000;
 }
