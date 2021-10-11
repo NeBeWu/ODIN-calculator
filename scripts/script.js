ce5c8db7c3184a30dda69c860d3e779e0a3fec35
nebewu <74262987+NeBeWu@users.noreply.github.com>
2021-10-11 16:21:42 -0300

    Implement remaining buttons functions
    Implement exponential and factorial operator buttons, implement
    decimal button, implement clear and delete buttons, and refactor
    the update variables functions.
    
    Except for the exponential and factorial buttons, every other
    button is a request of the project. The exponential and factorial
    buttons expand the calculator operations with the factorial only
    working for non-negative integers. The clear button resets all
    current variables and the display while the delete button just
    delete the last input of these. The decimal button allows to input
    floating points and don't allow to use more than one for the same
    number. Lastly, the refactor separates the update function and
    the append function from the number variables.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
+//Global variables to keep track of the current computation
 let firstNumber = '';
 let secondNumber = '';
 let operator = '';
+let operationResult = '';
+let divideByZero = 0;
 
+//Global constants that store the main nodes
 const currentDisplay = document.querySelector('.current-display');
 const pastDisplay = document.querySelector('.past-display');
 const numberButton = document.querySelectorAll('.number-button');
@@ -11,6 +15,7 @@ const decimalButton = document.querySelector('.decimal-button');
 const clearButton = document.querySelector('.clear-button');
 const deleteButton = document.querySelector('.delete-button');
 
+//Functions to update the display
 function updateCurrentDisplay() {
   currentDisplay.textContent = firstNumber + operator + secondNumber;
 }
@@ -24,13 +29,21 @@ function updatePastDisplay(value) {
   pastDisplay.prepend(content);
 }
 
+function resetPastDisplay() {
+  while (pastDisplay.firstChild) {
+    pastDisplay.removeChild(pastDisplay.lastChild);
+  }
+}
+
+//Functions that change the global variables and execute other
+//functions when these change
 function updateFirstNumber(value) {
-  firstNumber += value;
+  firstNumber = value;
   updateCurrentDisplay();
 }
 
 function updateSecondNumber(value) {
-  secondNumber += value;
+  secondNumber = value;
   updateCurrentDisplay();
 }
 
@@ -39,18 +52,46 @@ function updateOperator(value) {
   updateCurrentDisplay();
 }
 
+function appendFirstNumber(appendValue) {
+  updateFirstNumber(firstNumber + appendValue);
+}
+
+function appendSecondNumber(appendValue) {
+  updateSecondNumber(secondNumber + appendValue);
+}
+
 function resetCurrentVariables() {
   firstNumber = '';
   secondNumber = '';
   operator = '';
 }
 
+//Function responsible to operate the current expression,
+//change the display and reset global variables
+function equalsTo() {
+  if (firstNumber == '' || (operator != '!' && secondNumber == '')) {
+  } else if (operator == '÷' && secondNumber == '0') {
+    return alert('Watch out! You are trying to divide a number by 0.');
+  } else {
+    operationResult = operate(operator, firstNumber, secondNumber);
+    updatePastDisplay(operationResult);
+    resetCurrentVariables();
+    if (operationResult == 'Undefined') {
+      updateFirstNumber('');
+    } else {
+      updateFirstNumber(`${operationResult}`);
+    }
+  }
+}
+
+//EventListener assignment for each button
 numberButton.forEach((currentValue) => {
   currentValue.addEventListener('click', () => {
     if (operator == '') {
-      updateFirstNumber(currentValue.textContent);
+      appendFirstNumber(currentValue.textContent);
+    } else if (operator == '!') {
     } else {
-      updateSecondNumber(currentValue.textContent);
+      appendSecondNumber(currentValue.textContent);
     }
   });
 });
@@ -59,17 +100,82 @@ operatorButton.forEach((currentValue) => {
   currentValue.addEventListener('click', () => {
     if (firstNumber != '' && secondNumber == '') {
       updateOperator(currentValue.textContent);
+    } else if (firstNumber != '' && secondNumber != '') {
+      equalsTo();
+      updateOperator(currentValue.textContent);
     }
   });
 });
 
 equalsButton.addEventListener('click', () => {
-  const value = operate(operator, firstNumber, secondNumber);
-  updatePastDisplay(value);
-  resetCurrentVariables();
-  updateFirstNumber(`${value}`);
+  equalsTo();
 });
 
+clearButton.addEventListener('click', () => {
+  resetCurrentVariables();
+  updateCurrentDisplay();
+  resetPastDisplay();
+});
+
+decimalButton.addEventListener('click', () => {
+  if (firstNumber != '' && operator == '' && !firstNumber.includes('.')) {
+    appendFirstNumber(decimalButton.textContent);
+  } else if (secondNumber != '' && !secondNumber.includes('.')) {
+    appendSecondNumber(decimalButton.textContent);
+  }
+});
+
+deleteButton.addEventListener('click', () => {
+  if (firstNumber != '' && operator == '') {
+    updateFirstNumber(firstNumber.slice(0, firstNumber.length - 1));
+  } else if (operator != '' && secondNumber == '') {
+    updateOperator('');
+  } else if (secondNumber != '') {
+    updateSecondNumber(secondNumber.slice(0, secondNumber.length - 1));
+  }
+});
+
+//Function responsible to round answers when they get big
+function formatResult(number) {
+  if (number.toString().length > 10) {
+    return number.toPrecision(10);
+  } else {
+    return number;
+  }
+}
+
+//Functions corresponding to the calculator basic operations
+function add(number1, number2) {
+  return formatResult(number1 + number2);
+}
+
+function subtract(number1, number2) {
+  return formatResult(number1 - number2);
+}
+
+function multiply(number1, number2) {
+  return formatResult(number1 * number2);
+}
+
+function divide(number1, number2) {
+  return formatResult(number1 / number2);
+}
+
+function exponential(number1, number2) {
+  return formatResult(Math.pow(number1, number2));
+}
+
+function factorial(number) {
+  if (Number.isInteger(number) && number >= 0) {
+    let result = 1;
+    for (i = 1; i < number; i++) {
+      result *= i + 1;
+    }
+    return result;
+  } else return 'Undefined';
+}
+
+//Function responsible to operate an expression
 function operate(operator, number1, number2) {
   number1 = Number(number1);
   number2 = Number(number2);
@@ -83,27 +189,11 @@ function operate(operator, number1, number2) {
       return multiply(number1, number2);
     case '÷':
       return divide(number1, number2);
-    default:
-      return null;
+    case '^':
+      return exponential(number1, number2);
+    case '!':
+      return factorial(number1);
+    case '':
+      return number1;
   }
-}
-
-function add(number1, number2) {
-  return approximate(number1 + number2);
-}
-
-function subtract(number1, number2) {
-  return approximate(number1 - number2);
-}
-
-function multiply(number1, number2) {
-  return approximate(number1 * number2);
-}
-
-function divide(number1, number2) {
-  return approximate(number1 / number2);
-}
-
-function approximate(number) {
-  return Math.trunc(10000000000 * number) / 10000000000;
 }
